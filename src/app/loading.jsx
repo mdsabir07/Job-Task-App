f488cb62069e1c1f8b6954699b2e820fa2176855
mdsabir07 <sabiruli@gmail.com>
2025-08-23 09:24:41 +0600

    Fixed loading component issue

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,6 @@ export default function Loading() {
     return (
         <div className="flex justify-center items-center min-h-screen">
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500">
-                <p className="text-center py-8 text-lg">Loading...</p>
             </div>
         </div>
     );
